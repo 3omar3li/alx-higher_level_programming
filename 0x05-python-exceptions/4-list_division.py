55719b961c3defd7b51f2f6a21d8b0085fb77ca1
3omar3li <omar.ali2332@gmail.com>
2023-11-20 14:38:53 -0800

    4. Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,7 +9,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print('division by 0')
             continue
         except IndexError:
-            new_list,append(0)
+            new_list.append(0)
             print('out of range')
             continue
         except TypeError:
