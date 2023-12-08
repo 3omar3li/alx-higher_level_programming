f65244a9f0b02b7249822b23719f808944933ca0
3omar3li <omar.ali2332@gmail.com>
2023-12-08 14:45:11 -0800

    11. Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Module for rectangle class'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''A subclass representing a rectangle'''
+    def __init__(self, size):
+        '''Constructor'''
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        '''Method for area of square'''
+        return '[Square] ' + str(self.__size) + '/' str(self.__size)
