1b13d10d6ad1e90c7e0138098c13f16ffa5a0a19
3omar3li <omar.ali2332@gmail.com>
2023-12-08 15:20:01 -0800

    1. Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def number_of_lines(filename=""):
     '''Reads filename with uft-8'''
     with open(filename, "w", encoding='utf-8') as f:
-        return f.write((text))
+        return f.write(text)
