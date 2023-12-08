c6fa4e56cc636e190d47f5df19b01f213af09103
3omar3li <omar.ali2332@gmail.com>
2023-12-08 07:23:31 -0800

    0. Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-'''Module for lookup method.'''
-
 def lookup(obj):
-    '''Looks yp object attributesand methods.
-    Args:
-        obj (object): the object to list.
-
-    Returns
-    list: the list of available attributes and methods of an object
-    '''
     return dir(obj)
